46ef90255ec5810bb1af8fffed5cd3e70b55e8a0
Negar Lashgari <negar.lashgari@gmail.com>
2023-10-17 19:26:48 -0400

    recursion examples

---
+++ recursion.js
@@ -0,0 +1,12 @@
+const countdown = (x) => {
+  if (x == 0) {
+    console.log("done!");
+    return;
+  } else {
+    console.log("x is ", x);
+    countdown(x - 1); //recursive call = function calls itself
+    console.log("hi");
+  }
+};
+
+countdown(4);
