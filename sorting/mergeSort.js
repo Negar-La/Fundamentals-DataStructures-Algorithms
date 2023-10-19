1c0d8a793e12348053db138710cb5d6941e1fa5d
Negar Lashgari <negar.lashgari@gmail.com>
2023-10-18 21:00:57 -0400

    sorting examples

---
+++ sorting/mergeSort.js
@@ -0,0 +1,55 @@
+const mergeSort = (dataset) => {
+  //breaking condition
+  if (dataset.length <= 1) {
+    return dataset;
+  }
+
+  // Find the midpoint of the dataset
+  const middle = Math.floor(dataset.length / 2);
+
+  // Split the dataset into left and right arrays
+  const left = dataset.slice(0, middle);
+  const right = dataset.slice(middle);
+
+  // Recursively call mergeSort on the left and right arrays
+  const leftSorted = mergeSort(left);
+  const rightSorted = mergeSort(right);
+
+  // Merge the left and right arrays
+  return merge(leftSorted, rightSorted);
+};
+
+const merge = (left, right) => {
+  let i = 0;
+  let j = 0;
+  let merged = [];
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      merged.push(left[i]);
+      i++;
+    } else {
+      merged.push(right[j]);
+      j++;
+    }
+  }
+
+  // Add any remaining elements from the left and right arrays
+  while (i < left.length) {
+    merged.push(left[i]);
+    i++;
+  }
+
+  while (j < right.length) {
+    merged.push(right[j]);
+    j++;
+  }
+
+  return merged;
+};
+
+const unsortedList = [5, 2, 9, 3, 6, 1, 8];
+console.log("Unsorted dataset:", unsortedList);
+
+const sortedList = mergeSort(unsortedList);
+console.log("Sorted dataset:", sortedList);
